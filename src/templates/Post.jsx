556f85e5a8b4c25252a811532c7f174c6ea8eed7
Gabriele Destefanis <g.destefanis@topsolution.it>
2019-02-04 16:24:42 +0100

    Custom masonry gallery

--- src/templates/Post.jsx
+++ src/templates/Post.jsx
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import styled from '@emotion/styled'
-import {css} from '@emotion/core'
-import Isotope from 'isotope-layout'
+import { css } from '@emotion/core'
+import { debounce } from 'debounce'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import Card from '../components/Card'
 import PostMeta from '../components/PostMeta'
-import { SMALL_SCREEN_ONLY, MEDIUM_SCREEN_UP } from '../utils/breakpoints';
+import {
+  SMALL_SCREEN_ONLY,
+  MEDIUM_SCREEN_UP,
+  SMALL_SCREEN_MAX_SIZE
+} from '../utils/breakpoints';
 
+const galleryPadding = 5;
 const Article = styled.article`
-  padding: 1rem 2rem;
+  ${SMALL_SCREEN_ONLY} {
+    padding: 1rem;
+  }
+
+  ${MEDIUM_SCREEN_UP} {
+    padding: 1rem 2rem;
+  }
 
   h1 {
     text-align: center;
@@ -20,41 +31,47 @@ const Article = styled.article`
   }
 
   .wp-block-gallery {
-    margin-left: 0;
+    display: block;
+    width: 100%;
+  }
+
+  .gallery-wrapper {
+    margin: 1.5rem 0 3rem;
+    position: relative;
+    overflow: hidden;
     width: 100%;
 
-    .blocks-gallery-item {
-      display: block;
-      list-style: none;
-      padding: 5px;
+    .gallery-row {
+      margin-bottom: ${galleryPadding}px;
+    }
+
+    figure {
       margin: 0;
-
-      img {
-        display: block;
-        margin: 0;
-      }
-
-      figure {
-        position: relative;
-        overflow: hidden;
-      }
-
-      figcaption {
-        font-size: .8rem;
-        background: rgba(255, 255, 255, .6);
-        padding: 5px;
-        position: absolute;
-        bottom: -10em;
-        left: 0;
-        right: 0;
-        transition: bottom .3s;
-      }
+      position: absolute;
+      overflow: hidden;
 
       &:hover {
         figcaption {
           bottom: 0;
         }
       }
+    }
+
+    img {
+      margin: 0;
+      width: 100%;
+      display: block;
+    }
+
+    figcaption {
+      font-size: .8rem;
+      background: rgba(255, 255, 255, .6);
+      padding: ${galleryPadding}px;
+      position: absolute;
+      bottom: -10em;
+      left: 0;
+      right: 0;
+      transition: bottom .3s;
     }
   }
 
@@ -123,22 +140,73 @@ const featuredMediaSyle = css`
   }
 `;
 
+const calculateGalleryItemXoffset = (item, rowHeight) => {
+  if (!item || !item.prevRowItem) {
+    return 0;
+  }
+  return item.prevRowItem.ratio * rowHeight + galleryPadding + calculateGalleryItemXoffset(item.prevRowItem, rowHeight);
+}
+
+
 class PageTemplate extends Component {
+  drawGallery(galleryRows, prevGallery) {
+    const galleryWrapper = document.createElement('div');
+    galleryWrapper.className = 'gallery-wrapper';
+    galleryRows.forEach(row => {
+      row.height = (prevGallery.getBoundingClientRect().width - galleryPadding * (row.items.length - 1)) / row.ratio;
+      const galleryRow = document.createElement('div');
+      galleryRow.className = 'gallery-row';
+      galleryRow.style.height = `${row.height}px`;
+      row.items.forEach(item => {
+        item.element.style.height = `${row.height}px`;
+        item.element.style.width = `${row.height * item.ratio}px`;
+        item.element.style.transform = `translate(${calculateGalleryItemXoffset(item, row.height)}px, 0)`;
+        galleryRow.appendChild(item.element);
+      });
+      galleryWrapper.appendChild(galleryRow);
+    })
+    prevGallery.parentElement.replaceChild(galleryWrapper, prevGallery);
+    return galleryWrapper;
+  };
+
+  parseWPGallery(wpGallery, rowRatio) {
+    const items = wpGallery.querySelectorAll('figure');
+    const galleryRows = [];
+    let prevRowItem;
+    items.forEach(element => {
+      const { width, height } = element.childNodes[0];
+      const ratio = width / height;
+      if (galleryRows.length === 0 || Math.round(galleryRows[galleryRows.length - 1].ratio + ratio) > rowRatio) {
+        galleryRows.push({ items: [], ratio: 0 });
+        prevRowItem = null;
+      }
+      const row = galleryRows[galleryRows.length - 1];
+      const item = {element, width, height, ratio, prevRowItem}
+      row.items.push(item);
+      prevRowItem = item;
+      row.ratio += ratio;
+      element.parentElement.remove();
+    });
+    return galleryRows;
+  }
+
+  redrawGalleries() {
+    this.galleries = this.galleries.map(({rows, container}) => ({
+      rows,
+      container: this.drawGallery(rows, container)
+    }));
+  }
+
   componentDidMount() {
+    window.onresize = debounce(() => this.redrawGalleries(), 200);
+    const rowRatio = document.documentElement.clientWidth <= SMALL_SCREEN_MAX_SIZE ? 3 : 4;
+    this.galleries = [];
     document.querySelectorAll('.wp-block-gallery')
-      .forEach(element => {
-        // const items = element.childNodes;
-        // if (items.length > 1) {
-        //   items.forEach(item => {
-        //     item.style.width = items.length > 2 ? '33%' : '50%';
-        //   });
-        // }
-        new Isotope(element, {
-          itemSelector: '.blocks-gallery-item',
-          layoutMode: 'masonry',
-          masonry: {
-            // columnWidth: 50
-          }
+      .forEach(wpGallery => {
+        const rows = this.parseWPGallery(wpGallery, rowRatio);
+        this.galleries.push({
+          rows,
+          container: this.drawGallery(rows, wpGallery)
         });
       });
   }
