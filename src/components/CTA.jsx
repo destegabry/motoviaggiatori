2c7883017e1808a5b95be24304907412d7fc88e0
Gabriele Destefanis <g.destefanis@topsolution.it>
2019-03-04 18:45:29 +0100

    Added Instagram logo on CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,14 +6,23 @@ import { altFontStack } from '../utils/theme'
 export default styled.a`
   background: ${palette.primary.main};
   color: ${Color(palette.primary.contrast).alpha(.65).string()};
-  display: inline.block;
+  display: inline-block;
   font-family: ${altFontStack.join(', ')};
   text-transform: uppercase;
   padding: .5rem 1rem;
   transition: color .3s;
 
+  svg {
+    fill: ${Color(palette.primary.contrast).alpha(.65).string()};
+    transition: fill .3s;
+  }
+
   &:hover {
     color: ${palette.primary.contrast};
     box-shadow: none;
+
+    svg {
+      fill: ${palette.primary.contrast};
+    }
   }
 `
--- src/pages/foto.jsx
+++ src/pages/foto.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import {css} from '@emotion/core'
 import Layout from '../components/Layout'
 import Card from '../components/Card'
 import SEO from '../components/seo'
 import InstagramFeed from '../components/InstagramFeed'
 import CTA from '../components/CTA'
+import Instagram from '../images/instagram.svg'
 
 const CTAWrapper = styled.p`
   padding: 2rem 0 1rem;
@@ -31,6 +33,12 @@ const FotoPage = () => (
             target="blank"
             rel="noopener noreferrer"
           >
+            <Instagram css={css`
+              height: 1.5rem;
+              height: 1.5rem;
+              margin-bottom: -0.3rem!important;
+              margin-right: .5rem;
+            `} />
             Seguici su Instagram
           </CTA>
         </CTAWrapper>
